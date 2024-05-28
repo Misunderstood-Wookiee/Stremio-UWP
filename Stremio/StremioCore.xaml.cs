9c715064b48429df1bea3a9a1750c0be002a3e7c
MoistDreams <22002023+Misunderstood-Wookiee@users.noreply.github.com>
2024-05-29 07:39:43 +1000

    The Stuff....

---
+++ Stremio/StremioCore.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace Stremio
+{
+    /// <summary>
+    /// Core Display Page for Stremio Content.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public static string DeviceForm { get; } // Get Device Info
+        public MainPage()
+        {
+            this.InitializeComponent();
+            Environment.SetEnvironmentVariable("WEBVIEW2_DEFAULT_BACKGROUND_COLOR", "151330"); // Control WebView2 default background colour
+        }
+
+    }
+}
